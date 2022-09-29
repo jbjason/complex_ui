c39fffe7c4d5c1306486125c90c29d90badedfd9
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-09-29 07:12:30 +0600

    Create screen__.dart

---
+++ lib/screens/furniture_ui/screen__.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Screen12 extends StatelessWidget {
+  const Screen12({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
